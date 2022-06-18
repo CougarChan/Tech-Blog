6023830be07577d7fdc6ae9e63fa859c090cb22f
CougarChan <gohan0136@gmail.com>
2022-06-18 02:10:06 -0400

    Pushing the first 3 models onto the main frame still working on it

---
+++ model/User.js
@@ -0,0 +1,37 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("./db/database-connection");
+const bcrypt = require("bcrypt");
+
+class User extends Model {
+  passwordCheck(userPassword) {
+    return bcrypt.compareSync(userPassword, this.password)
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique:true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [8],
+      },
+    },
+  },
